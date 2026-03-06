9e131bfbb66add2fa9e2e5286c124dde73168317
omid2007hope <omid85hope@gmail.com>
2026-03-06 17:33:19 +0100

    Update

--- Backend/Server.js
+++ Backend/Server.js
@@ -5,7 +5,7 @@ const connectDB = require("./database/MongoDB");
 
 connectDB()
   .then(() => {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 4000;
     app.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`);
     });
