d51c227d2feb14d68dd59a0b6a3625d90b964086
omid2007hope <omid85hope@gmail.com>
2026-05-01 19:07:40 +0200

    MagazinPage very first foundation

---
+++ Frontend/src/features/magazine/MagazinePage.jsx
@@ -0,0 +1,14 @@
+export default function MagazinePage() {
+  const magazineData = [
+    { id: 1, title: "Magazine 1", description: "Description of Magazine 1" },
+    { id: 2, title: "Magazine 2", description: "Description of Magazine 2" },
+    { id: 3, title: "Magazine 3", description: "Description of Magazine 3" },
+  ];
+  ``;
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full w-full bg-  ">
+      <h1 className="text-4xl font-bold mb-4">Magazine Page</h1>
+    </div>
+  );
+}
