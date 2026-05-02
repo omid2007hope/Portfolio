80482c3aabc8da233013973bf9cc11956647e861
omid2007hope <omid85hope@gmail.com>
2026-05-02 13:49:50 +0200

    Magazine Page Part 1

--- Frontend/src/features/magazine/MagazinePage.jsx
+++ Frontend/src/features/magazine/MagazinePage.jsx
@@ -1,14 +1,42 @@
+import { getAllContent } from "../../api/Magazine/Magazine_API";
+
 export default function MagazinePage() {
-  const magazineData = [
-    { id: 1, title: "Magazine 1", description: "Description of Magazine 1" },
-    { id: 2, title: "Magazine 2", description: "Description of Magazine 2" },
-    { id: 3, title: "Magazine 3", description: "Description of Magazine 3" },
+  const tempTestData = [
+    {
+      id: 1,
+      title: "test",
+      description: "This is just for testing",
+      date: "02-05-2026",
+      image: null,
+    },
   ];
-  ``;
+
+  const data = getAllContent();
+
+  const normalizeData = () => {
+    data.map((content) => {
+      return {
+        id: content.magazineId,
+        title: content.title,
+        description: content.description || "",
+        date: content.date,
+        image: content.photo || null,
+      };
+    });
+  };
 
   return (
-    <div className="flex flex-col items-center justify-center h-full w-full bg-  ">
+    <div className="flex flex-col items-center justify-center h-full w-full">
       <h1 className="text-4xl font-bold mb-4">Magazine Page</h1>
+
+      {tempTestData.title !== ""
+        ? tempTestData.map((content) => {
+            <div className="">{content.title}</div>;
+          })
+        : normalizeData.map((content) => {
+            <div className="">{content.title}</div>;
+          })}
     </div>
   );
 }
+``;
